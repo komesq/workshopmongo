2d5771a6412cfe5327bbaeb48b25fd8c24549938
Kepler Oliveira de Mesquita <kepleroliveira@yahoo.com.br>
2018-07-06 20:44:27 -0300

    Incluindo os arquivos

---
+++ src/main/java/net/olimes/wshopmongo/WshopmongoApplication.java
@@ -0,0 +1,12 @@
+package net.olimes.wshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WshopmongoApplication.class, args);
+	}
+}

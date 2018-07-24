380a1afa99ae3f93f4e7bb82311abaa1e1c21d09
Kepler Oliveira de Mesquita <kepleroliveira@yahoo.com.br>
2018-07-24 02:34:17 -0300

    Obtendo usuario por id

---
+++ src/main/java/net/olimes/wshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,28 @@
+package net.olimes.wshopmongo.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import net.olimes.wshopmongo.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(
+				System.currentTimeMillis(), 
+				status.value(), 
+				"Não encontrado", 
+				e.getLocalizedMessage(), 
+				request.getRequestURI()
+				);
+		return ResponseEntity.status(status).body(err);
+		
+	}
+}
--- src/main/java/net/olimes/wshopmongo/services/UserService.java
+++ src/main/java/net/olimes/wshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package net.olimes.wshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import net.olimes.wshopmongo.domain.User;
 import net.olimes.wshopmongo.repository.UserRepository;
+import net.olimes.wshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow( () -> new ObjectNotFoundException("Usuário não encontrado") );
+		
 	}
 }
